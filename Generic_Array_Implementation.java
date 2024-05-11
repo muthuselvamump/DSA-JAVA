d28c4b216ebc285be114c77928fea720f974651b
muthuselvamump <muthuselvamump@gmail.com>
2024-05-11 23:02:49 +0530

    arrray implementation

---
+++ Generic_Array_Implementation.java
@@ -0,0 +1,23 @@
+import java.util.*;
+public class Generic_Array_Implementation{
+    public static void main(String []args){
+       Generic_Array_Implementation ar=new Generic_Array_Implementation();
+        while(true){
+            System.out.println("1 for add Element");
+            System.out.println("2.delete Element");
+            System.out.println("5 for stop");
+            Scanner scan=new Scanner(System.in);
+            int arrnum=scan.nextInt();
+            switch(arrnum){
+                case 1:{
+                    System.out.println("hello");
+                    break;
+                }
+                case 2:{
+                    System.out.println("world");
+                    break;
+                }
+            }
+        }
+    }
+}
