8149fa7e172b1ee73219bfa5977dc698be28eaf1
muthuselvamump <muthuselvamump@gmail.com>
2024-06-01 23:02:41 +0530

    Linked_List_implementation

--- Linked_List_implementation.java
+++ Linked_List_implementation.java
@@ -4,7 +4,8 @@ class likedlist{
 }
 public class Linked_List_implementation {
     public static void main(String []args){
-
+        likedlist linklist=new likedlist();
+        isertatbegin(10);
     }
     
 }
