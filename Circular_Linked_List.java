1af4a60ace878d17fb12ebbd9b61970c46ecde8c
muthuselvamump <muthuselvamump@gmail.com>
2024-06-13 22:36:53 +0530

    Circular_List_implementation

--- Circular_Linked_List.java
+++ Circular_Linked_List.java
@@ -1,10 +1,18 @@
+class node{
+    
+}
 class Circular_linked_list{
+
 
 }
 public class Circular_Linked_List {
     public static void main(String[] args) {
         Circular_linked_list list1=new Circular_linked_list();
-        list1.insertat
+        list1.insertatbegin(8);
+
+
+
+
 
     }
     
