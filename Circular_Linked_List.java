1642557e9b27c28bbf0ed86dbd0d621449c1d17e
muthuselvamump <muthuselvamump@gmail.com>
2024-06-15 23:38:27 +0530

    Circular_List_implementation

--- Circular_Linked_List.java
+++ Circular_Linked_List.java
@@ -2,17 +2,41 @@ class node{
     node next;
     int data;
 }
-class Circular_linked_list{
+class Clist{
     node head;
-    public void Circular_linked_List(){
+    Clist(){
         head=null;
+    }
+    public void insertatbegin(int val1){
+        node temp=new node();
+        if(head==null){
+            temp.data=val1;
+            temp.next=head;
+            head=temp;
+        }
+        else{
+            temp.next=head;
+            head=temp;
+        }
+    
+    }
+    public void display(){
+     node temp=head;
+     while(temp.next==head){
+        System.out.println(temp.data);
+        temp=temp.next;
+     }   
     }
 
 }
 public class Circular_Linked_List {
     public static void main(String[] args) {
-        Circular_linked_list list1=new Circular_linked_list();
+        Clist list1=new Clist();
         list1.insertatbegin(8);
+        list1.insertatbegin(5);
+        list1.insertatbegin(12);
+        list1.insertatbegin(43);
+        list1.display();
 
 
 
