eba118072aa00898c5c5aa77f4fb167c5c816554
muthuselvamump <muthuselvamump@gmail.com>
2024-08-13 22:20:59 +0530

    Binary_Search

--- Binary_Search.java
+++ Binary_Search.java
@@ -3,6 +3,7 @@ class Binsearch{
         int start=0;
         int end=ar1.length-1;
        //int mid;
+       if(ar1[0]<ar1[ar1.length-1]){
         while(start<end){
             int mid=(start+end)/2;
             if(ar1[mid]>target){
@@ -16,12 +17,17 @@ class Binsearch{
                 return mid;
             }
         }
+    }
+    else{
+        
+    }
         return -1;
     }
 }
 public class Binary_Search {
     public static void main(String args[]){
         int arr[]={1,3,5,6,8,9,11,15};
+        int arr2[]={25,22,16,11,8,7,5,2,1};
     System.out.println(Binsearch.find(arr,9));
     }
     
