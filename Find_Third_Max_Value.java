60371095f1d0effc447027ef313b04bf30d3a8ad
muthuselvamump <muthuselvamump@gmail.com>
2024-08-14 23:08:15 +0530

    Find_Third_Max_Value

--- Find_Third_Max_Value.java
+++ Find_Third_Max_Value.java
@@ -1,7 +1,28 @@
+class thirdmax{
+    public static void find(int []arr){
+        int firstmax=0;
+        int semax=0;
+        int third=0;
+    for (int i=0;i<4;i++){
+        for(int j=0;j<arr.length;j++){
+            if(arr[j]>firstmax){
+                firstmax=arr[j];
+            }
+            if(arr[j]> semax && arr[j] != firstmax){
+                semax=arr[j];
+            }
+            if(arr[j]> third && arr[j] != firstmax && arr[j] != semax){
+                third=arr[j];
+            }
+        }
+    }
+        System.out.println(third);
+    }
+}
 public class Find_Third_Max_Value {
     public static void main(String []args){
 
-        int arr[]={2,55,4,77,33,22,33,1,5,6};
+        int arr[]={2,55,4,77,33,22,33,1,5,6,-55};
         thirdmax.find(arr);
     }
 }
