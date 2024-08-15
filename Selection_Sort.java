3912e64e88fbfdd45eefb80b4d45fc3ce6c49e8c
muthuselvamump <muthuselvamump@gmail.com>
2024-08-15 12:16:51 +0530

    Selection_Sort

--- Selection_Sort.java
+++ Selection_Sort.java
@@ -19,6 +19,7 @@ class selectsort{
 }
 public class Selection_Sort {
     public static void main(String []args){
-       
+        int []arr={3,1,5,23,76,2};
+        System.out.println(Arrays.toString(selectsort.sorting(arr)));
     }
 }
