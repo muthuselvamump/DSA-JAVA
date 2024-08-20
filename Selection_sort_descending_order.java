c13bc17b4ea76799b959aa2542d541f671d7d359
muthuselvamump <muthuselvamump@gmail.com>
2024-08-20 22:43:14 +0530

    Selection_sort_descending_order

---
+++ Selection_sort_descending_order.java
@@ -0,0 +1,11 @@
+class selectionsort{
+    public void descending(int []ar){
+        
+    }
+}
+public class Selection_sort_descending_order{
+    public static void main(String []args){
+        int []arr={3,1,45,3,6,77,15,1,2};
+        selectionsort.descending(arr);
+    }
+}
