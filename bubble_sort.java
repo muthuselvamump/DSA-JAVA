4eafe7b4520e19850ba59a9f330b6681ceb12e9f
muthuselvamump <muthuselvamump@gmail.com>
2024-08-21 22:22:46 +0530

    bubble_sort

--- bubble_sort.java
+++ bubble_sort.java
@@ -3,13 +3,19 @@ class bubble{
     public static void sort(int arr[]){
         int count=arr.length-1;
         int temp=0;
+        boolean flag;
         for(int i=0;i<arr.length-1;i++){
+            flag=false;
             for(int j=0;j<count;j++){
                 if(arr[j]>arr[j+1]){
                   temp=arr[j];
                   arr[j]=arr[j+1];
-                  arr[j+1]=temp;  
+                  arr[j+1]=temp;
+                  flag=true;  
                 }
+            }
+            if(!flag){
+                break;
             }
             count--;
         }
