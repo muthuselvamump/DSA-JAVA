1af083fdf4fc3323e4be955b05bdd321e1606b3c
muthuselvamump <muthuselvamump@gmail.com>
2024-08-21 21:52:17 +0530

    bubble_sort

--- bubble_sort.java
+++ bubble_sort.java
@@ -1,12 +1,25 @@
+import java.util.*;
 class bubble{
     public static void sort(int arr[]){
-
+        int count=arr.length-1;
+        int temp=0;
+        for(int i=0;i<arr.length-1;i++){
+            for(int j=0;j<count;j++){
+                if(arr[j]>arr[j+1]){
+                  temp=arr[j];
+                  arr[j]=arr[j+1];
+                  arr[j+1]=temp;  
+                }
+            }
+            count--;
+        }
     }
 }
 public class bubble_sort {
-    public static void main(String args){
-        int []p={31,1,5,32,1,4,2,55,33,22};
+    public static void main(String []args){
+        int []p={2,1,5,32,1,4,2,55,33,22};
         bubble.sort(p);
+        System.out.println(Arrays.toString(p));
     }
     
 }
