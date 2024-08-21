10ea670fc6be8a74c24fc73943289b893ab19e3f
muthuselvamump <muthuselvamump@gmail.com>
2024-08-21 21:16:45 +0530

    bubble_sort

---
+++ bubble_sort.java
@@ -0,0 +1,12 @@
+class bubble{
+    public static void sort(int arr[]){
+
+    }
+}
+public class bubble_sort {
+    public static void main(String args){
+        int []p={31,1,5,32,1,4,2,55,33,22};
+        bubble.sort(p);
+    }
+    
+}
