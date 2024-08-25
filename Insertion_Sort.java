2ae5c9880de6632ef92203b3ea34f8d5826f63f8
muthuselvamump <muthuselvamump@gmail.com>
2024-08-25 22:48:45 +0530

    Insertion_Sort

--- Insertion_Sort.java
+++ Insertion_Sort.java
@@ -1,10 +1,13 @@
+import java.util.*;
 class insertion{
  public static void sort(int arr[]){
     int temp=0;
-    for(int i=0;i<arr.length;i++){
-        for(int j=1;j<i;j--){
+    for(int i=1;i<arr.length;i++){
+        for(int j=i;j>0;j--){
             if(arr[j]<arr[j-1]){
-
+                temp=arr[j-1];
+                arr[j-1]=arr[j];
+                arr[j]=temp;
             }
         }
     }
@@ -14,5 +17,6 @@ public class Insertion_Sort{
     public static void main(String args[]){
         int arr[]={3,1,4,2,6,66,77,22,11};
         insertion.sort(arr);
+        System.out.println(Arrays.toString(arr));
     }
 }
