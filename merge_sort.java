1fcc4c450c3c4d9f0be3d62a758774882dbe894f
muthuselvamump <muthuselvamump@gmail.com>
2024-08-26 23:30:09 +0530

    merge_sort

--- merge_sort.java
+++ merge_sort.java
@@ -1,6 +1,13 @@
+class sorting{
+    public static int[] merge(int []arr){
+        if(arr.length==1){
+            return arr;
+        }
+    }
+}
 public class merge_sort {
     public static void main(String []args){
-        
+
     }
     
 }
