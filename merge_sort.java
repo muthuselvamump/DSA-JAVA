efbb50de40322e9cedc11b2b41902cd7a8f42432
muthuselvamump <muthuselvamump@gmail.com>
2024-08-27 22:14:53 +0530

    merge_sort

--- merge_sort.java
+++ merge_sort.java
@@ -1,8 +1,17 @@
+import java.util.Arrays;
+
 class sorting{
     public static int[] merge(int []arr){
         if(arr.length==1){
             return arr;
         }
+        int mid=arr.length/2;
+        int[] left=merge(Arrays.copyOfRange(arr,0, mid));
+        int[] right=merge(Arrays.copyOfRange(arr, mid, arr.length));
+        return mergesort(left,right);
+    }
+    public static int[] mergesort(int left[],int right[]){
+
     }
 }
 public class merge_sort {
