48228d74c32893f680f7bfe04d46f1fe6b1a46b5
muthuselvamump <muthuselvamump@gmail.com>
2024-08-28 23:36:00 +0530

    merge_sort_reduce_space_complexity

--- merge_sort_reduce_space_complexity.java
+++ merge_sort_reduce_space_complexity.java
@@ -1,6 +1,16 @@
+class mergesortspace{
+    public static void mergesortinplace(int arr[],int start,int end){
+        if(start-end==1){
+            return;
+        }
+        int mid=(start+end)/2;
+        mergesortinplace(arr, start, mid);
+        mergesortinplace(arr, mid, end);
+    }
+}
 public class merge_sort_reduce_space_complexity {
     public static void main(String []args){
-        
+
     }
     
 }
