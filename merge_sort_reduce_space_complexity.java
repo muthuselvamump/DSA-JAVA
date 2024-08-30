e1b30b250f7fc7a56f855a2a1568688cd190a5ac
muthuselvamump <muthuselvamump@gmail.com>
2024-08-30 23:23:29 +0530

    merge_sort_reduce_space_complexity

--- merge_sort_reduce_space_complexity.java
+++ merge_sort_reduce_space_complexity.java
@@ -1,19 +1,19 @@
 import java.util.*;
 class mergesortspace{
     public static void mergesortinplace(int arr[],int start,int end){
-        if(start-end<=1){
+        if(end-start==1){
             return;
         }
         int mid=(start+end)/2;
         mergesortinplace(arr, start, mid);
         mergesortinplace(arr, mid, end);
-        mergeing(arr, start, end, mid);
+        mergeing(arr, start, mid,end );
     }
-    public static void mergeing(int arr[],int st,int en,int mi){
-        int k[]=new int[st-en];
+    public static void mergeing(int arr[],int st,int mi,int en){
+        int k[]=new int[en-st];
         int i=st,k1=0,j=mi;
         while(i < mi && j<en){
-            if(arr[i]<=arr[j]){
+            if(arr[i]<arr[j]){
                 k[k1++]=arr[i++];
             }
             else{
@@ -27,14 +27,15 @@ class mergesortspace{
         while(j<en){
             k[k1++]=arr[j++];
         }
+       
 
     }
 }
 public class merge_sort_reduce_space_complexity {
     public static void main(String []args){
-        int aq1[]={2,1,3,4,2,5,6,2,1};
-        mergesortspace.mergesortinplace(aq1, 0, aq1.length);
-        System.out.println(Arrays.toString(aq1));
+        int arr[]={2,1,3,4,2,5,6,2,1};
+        mergesortspace.mergesortinplace(arr, 0, arr.length);
+        System.out.println(Arrays.toString(arr));
     }
     
 }
