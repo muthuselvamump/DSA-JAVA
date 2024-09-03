00b694d95c7f80ab02403aa98302d508a07bafd3
muthuselvamump <muthuselvamump@gmail.com>
2024-09-04 00:07:43 +0530

    Quick_Sort

--- Quick_Sort.java
+++ Quick_Sort.java
@@ -1,6 +1,11 @@
+class quicksort{
+    public static void qsort(int[] arr,int low,int high){
+        
+    }
+}
 public class Quick_Sort {
     public static void main(String []args){
-        
+
     }
     
 }
