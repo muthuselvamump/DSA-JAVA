8df7a51d8328ace1b9ce29d682b9ce4c1dc950e1
muthuselvamump <muthuselvamump@gmail.com>
2024-09-04 22:14:52 +0530

    Quick_Sort

--- Quick_Sort.java
+++ Quick_Sort.java
@@ -1,3 +1,4 @@
+import java.util.Arrays;
 class quicksort{
     public static void qsort(int[] arr,int low,int high){
         
@@ -5,7 +6,9 @@ class quicksort{
 }
 public class Quick_Sort {
     public static void main(String []args){
-
+        int arr[]={2,1,4,5,3,2,5,8,1};
+        quicksort.qsort(arr, 0, arr.length-1);
+        System.out.println(Arrays.toString(arr));
     }
     
 }
