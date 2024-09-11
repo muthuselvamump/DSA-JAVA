45deaebba4a739ead43f51510a8b763e0a97b2d8
muthuselvamump <muthuselvamump@gmail.com>
2024-09-11 23:25:46 +0530

    quiz_tricky_byte

---
+++ Binarytreedemo.java
@@ -0,0 +1,12 @@
+class btree{
+    class nodebree{
+        nodebree left;
+        nodebree right;
+        int Data;
+    }
+}
+public class Binarytreedemo {
+    public static void main(String args[]){
+
+    }
+}
