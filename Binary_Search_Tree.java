c4caf6532b6702d7b103eba86f86658571a2fc27
muthuselvamump <muthuselvamump@gmail.com>
2024-09-16 22:45:09 +0530

    Binary_Search_Tree

--- Binary_Search_Tree.java
+++ Binary_Search_Tree.java
@@ -1,7 +1,19 @@
 class bsearch{
-    class nodeb{
-        
+    class nodebs{
+        int key;
+        nodebs left;
+        nodebs right;
+        public nodebs(int val){
+            key=val;
+            left=null;
+            right=null;
+        }
     }
+    nodebs root;
+    public bsearch(int val){
+        root=new nodebs(val);
+    }
+    
 }
 public class Binary_Search_Tree {
     public static void main(String args[]){
