df9386b243450b00a1ff6af9c19b6e8fbcab3b17
muthuselvamump <muthuselvamump@gmail.com>
2024-12-19 23:19:34 +0530

    Binary_Search_Tree

--- Binary_Search_Tree.java
+++ Binary_Search_Tree.java
@@ -15,6 +15,27 @@ class bsearch{
     }
     public void insert(int val){
         insert(root,val);
+        //System.out.println("return value"+temp.key);
+    }
+    public void search(nodebs root,int val){
+        nodebs temp=root;
+        int flag=0;
+        while(temp !=null){
+            if(temp.key>val){
+                temp=temp.left;
+            }
+            else{
+                temp=temp.right;
+            }
+            if(temp!=null && temp.key==val){
+                System.out.println("found");
+                flag=1;
+            }
+        }
+        if(flag==0){
+            System.out.println("not found");
+        }
+
     }
     public nodebs insert(nodebs root,int val){
         if(root ==null){
@@ -26,8 +47,9 @@ class bsearch{
         else{
             root.left=insert(root.left,val);
         }
-
+        //nodebs test=new nodebs(5);
         return root;
+        
     }
     public void inorder(nodebs root){
         if(root == null){
@@ -43,13 +65,14 @@ public class Binary_Search_Tree {
     public static void main(String args[]){
         bsearch n=new bsearch(50);
         n.insert(40);
-        n.insert(20);
+       n.insert(20);
         n.insert(25);
         n.insert(10);
         n.insert(70);
         n.insert(60);
         n.insert(90);
         n.inorder(n.root);
+      
 
 
 
