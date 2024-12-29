e212bf951bc8cb95eeae4cd21f3886afd5f75623
muthuselvamump <muthuselvamump@gmail.com>
2024-12-29 21:20:44 +0530

    AVL_Tree

--- AVL_Tree.java
+++ AVL_Tree.java
@@ -18,23 +18,48 @@ public nodeavl(int val){
 }
 
   }
+  public void insert(int val){
+    insert(root ,val);
+  }
 public nodeavl insert(nodeavl root,int val){
     if(root==null){
         return new nodeavl(val);
     }
     if(val>root.key){
-        root.left=
+        root.right=insert(root.right,val);
+    }
+    if(val<root.key){
+        root.left=insert(root.left,val);
     }
     return root;
 }
-
-
+public nodeavl inorder(nodeavl root){
+    if(root !=null){
+        inorder(root.left);
+        System.out.print(root.key+ " " );
+        inorder(root.right);
+    }
+    return root;
+}
 
 
 }
 
 public class AVL_Tree {
     public static void main(String args[]){
+        avltree n1=new avltree(50);
+        n1.insert(40);
+       n1.insert(20);
+        n1.insert(25);
+        n1.insert(10);
+        n1.insert(70);
+        n1.insert(60);
+        n1.insert(90);
+        n1.insert(85);
+        n1.insert(95);
+        n1.insert(75);
+        n1.insert(96);
+        n1.inorder(n1.root);
 
     }
     
