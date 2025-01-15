3d4832c86b99d337a0ee4d9d5ec3412a744fca93
muthuselvamump <muthuselvamump@gmail.com>
2025-01-15 23:21:27 +0530

    graph

--- Graph_refrenctace.java
+++ Graph_refrenctace.java
@@ -1,11 +1,12 @@
 public class Graph_refrenctace {
     public static void main(String args[]){
         int arr[][]={
-        {},
-        {},
-        {},
-        {}};
-
+        {0,1,0,0,1},
+        {1,0,1,0,1},
+        {0,1,0,1,0},
+        {0,0,1,0,1},
+        {1,1,0,1,0}};
+        System.out.println("Adjecency matrix"+ " "+ arr[0][1]);
     }
     
 }
